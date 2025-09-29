e3d00a6930dcc062dd00b3eaad608ef34617b6d3
João Carias <joao.carias@vsb.cz>
2025-09-29 11:03:10 +0200

    Creating parquet file for v001 dataset.

--- src/ensemble/ensemble.py
+++ src/ensemble/ensemble.py
@@ -3,6 +3,8 @@ from ensemble.model_ae_v1 import Autoencoder
 
 #import torch
 #import lightning as pl
+import pandas as pd
+import os
 from pathlib import Path
 from os.path import join
 import numpy as np
@@ -111,7 +113,7 @@ def create_analysis_dataset(qa_dataset_dataframe_path: str, output_path: str) ->
 
 def build_initial_dataset_v001(
         qa_dataset_dataframe_path: str, 
-        output_path: str, 
+        output_path: str,
         crop_size: int = 64, 
         apply_blue_layer: bool = True
         ) -> None: 
@@ -125,8 +127,17 @@ def build_initial_dataset_v001(
     and the cropped gt image as 2nd layer.
 
     """
-    # create output path if it doesn't exist
-    Path(output_path).mkdir(parents=True, exist_ok=True)
+    # dataset id
+    dataset_id = "v001"
+    composed_output_path = os.path.join(output_path, dataset_id)
+    # destination path of the created images
+    images_output_path = os.path.join(composed_output_path, "images")
+    # create images path if it doesn't exist
+    Path(images_output_path).mkdir(parents=True, exist_ok=True)
+
+    # output parquet support file
+    data_list = []
+
     # loads the QA dataset
     df = load_dataframe_from_parquet_with_metadata(qa_dataset_dataframe_path)
     # get the gt images
@@ -137,6 +148,7 @@ def build_initial_dataset_v001(
     # sets the size of the array that will contain the summed images
     canvas_size = qa_crop_size * 4
     canvas_half_size = canvas_size // 2
+
     # go through the gt images
     for gt_image_path in tqdm(
         unique_gt_images, total=len(unique_gt_images), desc="Iterating over gt"
@@ -147,6 +159,7 @@ def build_initial_dataset_v001(
         gt_image = tifffile.imread(gt_image_path)
         # find the different labels in a gt
         labels = np.unique(gt_image)[1:]
+
         # go through each label
         for label in labels:
             # dataframe with competitors segmentation for the same cell
@@ -157,6 +170,7 @@ def build_initial_dataset_v001(
             first_row = df_same_cell.iloc[0]
             # gets the original center that is used to center and overlap the competitors segmentations
             qa_crop_original_center_y, qa_crop_original_center_x = first_row["original_center_y"], first_row["original_center_x"]
+
             # go through the competitors segmentations and add them to canvas
             for row in df_same_cell.itertuples():
                 # load competitor segmentation from qa
@@ -166,6 +180,7 @@ def build_initial_dataset_v001(
                 start_x = canvas_half_size - qa_crop_half_size + row.original_center_x - qa_crop_original_center_x
                 # create an image that is the sum of all segmentations of a label
                 canvas[start_y : start_y + qa_crop_size, start_x : start_x + qa_crop_size] += competitor_qa_image
+
             # normalized competitors sumation
             canvas = (canvas // len(df_same_cell)).astype(np.uint8)
             # find the center of the image summation
@@ -173,6 +188,7 @@ def build_initial_dataset_v001(
             obj_slice_y, obj_slice_x = find_objects(canvas_mask)[0]
             obj_center_y = obj_slice_y.start + ((obj_slice_y.stop - obj_slice_y.start) // 2)
             obj_center_x = obj_slice_x.start + ((obj_slice_x.stop - obj_slice_x.start) // 2)
+
             # crop canvas
             crop_half_size = crop_size // 2
             obj_min_y, obj_min_x = obj_center_y - crop_half_size, obj_center_x - crop_half_size
@@ -183,18 +199,39 @@ def build_initial_dataset_v001(
             gt_crop = gt_image[gt_crop_min_y : gt_crop_min_y + crop_size, 
                                gt_crop_min_x : gt_crop_min_x + crop_size]
             gt_crop = (gt_crop == label).astype(np.uint8) * 255
+
             # stack layers
             if apply_blue_layer:
                 empty_blue_layer = np.zeros((crop_size, crop_size), dtype=np.uint8)
                 stacked_crop = np.stack([canvas_crop, gt_crop, empty_blue_layer], axis=0)
             else:
                 stacked_crop = np.stack([canvas_crop, gt_crop], axis=0)
+
             # set new dataset image path
-            campain, img_id, __, __ = first_row.cell_id.split("_")
-            new_image_name = f"{campain}_{img_id}_{label}.tif"
-            new_image_path = f"{output_path}/{new_image_name}"
+            campaign, img_id, __, __ = first_row.cell_id.split("_")
+            new_image_name = f"{campaign}_{img_id}_{label}.tif"
+            new_image_path = os.path.join(images_output_path, new_image_name)
             # save image
             tifffile.imwrite(new_image_path, stacked_crop)
+
+            # save details
+            data_list.append(
+                {
+                    "campaign": campaign,
+                    "image_id": img_id,
+                    "label": label,
+                    "crop_size": crop_size,
+                    "image_path": new_image_path,
+                }
+            )
+
+    # convert list to dataframe
+    output_df = pd.DataFrame(data_list)
+    # build output parquet path
+    parquet_output_path = os.path.join(composed_output_path, f"ensemble_dataset_{dataset_id}.parquet")
+    # save to parquet file
+    output_df.to_parquet(parquet_output_path)
+
 
 
 
