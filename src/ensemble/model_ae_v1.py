960db36ee71dd065e20ea325f8ae1859cab19606
João Carias <joao.carias@vsb.cz>
2025-10-17 17:12:57 +0200

    Added resize transform. Smaller code arrangements.

--- src/ensemble/model_ae_v1.py
+++ src/ensemble/model_ae_v1.py
@@ -1,9 +1,16 @@
+# AE based: https://lightning.ai/docs/pytorch/stable/notebooks/course_UvA-DL/08-deep-autoencoders.html
+# VAE based: https://github.com/dariocazzani/pytorch-AE/blob/master/models/VAE.py
+
 import torch
 from torch import nn, optim
+from torch.nn import functional as F
 import pytorch_lightning as pl
 from pytorch_lightning.utilities.types import OptimizerLRSchedulerConfig
 from collections.abc import Callable
+
+from torchmetrics import JaccardIndex
 from src.ensemble.models_loss_type import LossType
+from torchvision import transforms
 
 
 class Encoder32(nn.Module):
@@ -19,6 +26,7 @@ class Encoder32(nn.Module):
         """
         super().__init__()
         c_hid = base_channel_size
+        self.transform = transforms.Compose([transforms.Resize((32, 32)),])
         self.net = nn.Sequential(
             nn.Conv2d(num_input_channels, c_hid, kernel_size=3, padding=1, stride=2),  # 32x32 => 16x16
             act_fn(),
@@ -35,11 +43,12 @@ class Encoder32(nn.Module):
         )
 
     def forward(self, x):
+        x = self.transform(x)
         return self.net(x)
 
 
 class Decoder32(nn.Module):
-    def __init__(self, num_input_channels: int, base_channel_size: int, latent_dim: int, act_fn: Callable = nn.GELU):
+    def __init__(self, base_channel_size: int, latent_dim: int, last_act_fn: Callable, act_fn: Callable = nn.GELU):
         """Decoder.
 
         Args:
@@ -50,6 +59,8 @@ class Decoder32(nn.Module):
 
         """
         super().__init__()
+        self.transform = transforms.Compose([transforms.Resize((64, 64)),])
+        num_outputs = 1 # the output is a single "grayscale" image
         c_hid = base_channel_size
         self.linear = nn.Sequential(nn.Linear(latent_dim, 2 * 16 * c_hid), act_fn())
         self.net = nn.Sequential(
@@ -61,16 +72,15 @@ class Decoder32(nn.Module):
             act_fn(),
             nn.Conv2d(c_hid, c_hid, kernel_size=3, padding=1),
             act_fn(),
-            nn.ConvTranspose2d(c_hid, num_input_channels, kernel_size=3, output_padding=1, padding=1, stride=2),  # 16x16 => 32x32
-            nn.Tanh(),  # The input images is scaled between -1 and 1, hence the output has to be bounded as well
-            #nn.GELU(),
-            #nn.Sigmoid(),
+            nn.ConvTranspose2d(c_hid, num_outputs, kernel_size=3, output_padding=1, padding=1, stride=2),  # 16x16 => 32x32
+            last_act_fn(),  # Tanh() for MSE, Sigmoid() for BCE
         )
 
     def forward(self, x):
         x = self.linear(x)
         x = x.reshape(x.shape[0], -1, 4, 4)
         x = self.net(x)
+        x = self.transform(x)
         return x
 
 class Encoder64(nn.Module):
@@ -107,7 +117,7 @@ class Encoder64(nn.Module):
 
 
 class Decoder64(nn.Module):
-    def __init__(self, num_channels: int, latent_dim: int, act_fn: Callable = nn.GELU):
+    def __init__(self, num_channels: int, latent_dim: int, last_act_fn: Callable, act_fn: Callable = nn.GELU):
         """Decoder.
 
         Args:
@@ -134,10 +144,7 @@ class Decoder64(nn.Module):
                2 * num_channels, num_channels, kernel_size=3, output_padding=1, padding=1, stride=2),  # 16x16 => 32x32
             nn.ConvTranspose2d(
                num_channels, num_outputs, kernel_size=3, output_padding=1, padding=1, stride=2),  # 32x32 => 64x64
-            nn.Tanh(), # The input images is scaled between -1 and 1, hence the output has to be bounded as well
-            #nn.GELU(),
-            #nn.Sigmoid(),
-
+            last_act_fn(), # Tanh() for MSE, Sigmoid() for BCE
         )
 
     def forward(self, x):
@@ -155,22 +162,30 @@ class Autoencoder_v1(pl.LightningModule):
         latent_dim: int,
         input_size: int,
         loss_type: LossType,
+        is_variational: bool = False
     ):
         super().__init__()
-        # Saving hyperparameters of autoencoder
-        self.save_hyperparameters()
+
+        if loss_type == LossType.MSE:
+            last_decoder_act_fn = nn.Tanh
+            self.loss_function = F.mse_loss
+        elif loss_type == LossType.BCE:
+            last_decoder_act_fn = nn.Sigmoid
+            self.loss_function = F.binary_cross_entropy
+
         # Creating encoder and decoder
         if input_size == 32:
             self.encoder = Encoder32(num_inputs, num_channels, latent_dim)
-            self.decoder = Decoder32(num_inputs, num_channels, latent_dim)
+            self.decoder = Decoder32(num_channels, latent_dim, last_decoder_act_fn)
         elif input_size == 64:
             self.encoder = Encoder64(num_inputs, num_channels, latent_dim)
-            self.decoder = Decoder64(num_channels, latent_dim)
+            self.decoder = Decoder64(num_channels, latent_dim, last_decoder_act_fn)
         else:
             raise Exception("This model only supports input_size of 32 or 64.") 
         # Example input array needed for visualizing the graph of the network
         self.example_input_array = torch.zeros(3, num_inputs, input_size, input_size)
-        self.loss_type = loss_type
+        # Saving hyperparameters of autoencoder
+        self.save_hyperparameters()
 
     def forward(self, x):
         """The forward function takes in an image and returns the reconstructed image."""
@@ -182,16 +197,8 @@ class Autoencoder_v1(pl.LightningModule):
         """Given a batch of images, this function returns the reconstruction loss."""
         x, y = batch
         x_hat = self.forward(x)
-
-        if self.loss_type == LossType.MSE:
-            loss = nn.functional.mse_loss(x_hat, y, reduction="none")
-            loss = loss.sum(dim=[1, 2, 3]).mean(dim=[0])
-
-        elif self.loss_type == LossType.BCE:
-            loss = nn.functional.binary_cross_entropy(x_hat, y, reduction="none")
-            loss = loss.sum(dim=[1, 2, 3]).mean(dim=[0])
-
-        return loss # type: ignore
+        loss = self.loss_function(x_hat, y, reduction="sum")
+        return loss
 
     def configure_optimizers(self):
         optimizer = optim.Adam(self.parameters(), lr=1e-3)
