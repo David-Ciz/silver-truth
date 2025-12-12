d139acdf62532fed7e5bbc0fb5ca85b1deabd5ee
João Carias <joao.carias@vsb.cz>
2025-12-12 22:23:01 +0000

    Remove build_qa_dataset().

--- src/ensemble/external.py
+++ src/ensemble/external.py
@@ -7,17 +7,5 @@ def compress_images(image_folder, recursive=True):
     dp_comp.compress_tifs_logic(image_folder, recursive, False, False)
 
 
-def build_qa_dataset(
-    dataset_dataframe_path: str,
-    output_dir: str,
-    output_parquet_path: str,
-):
-    qa_pp.create_qa_dataset(dataset_dataframe_path, 
-                            output_dir,
-                            output_parquet_path, 
-                            crop=True, 
-                            crop_size=64)
-
-
 def load_parquet(input_path: str):
     return ddc.load_dataframe_from_parquet_with_metadata(input_path)
