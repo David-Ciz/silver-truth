41a1b172337c9962268c8b84fa8e129f6a8d9e85
jocarias <11091450+jocarias@users.noreply.github.com>
2026-02-22 22:10:33 +0000

    Model Dynamic with problems in backpropagation.

--- src/silver_truth/ensemble/model_unet_dynamic.py
+++ src/silver_truth/ensemble/model_unet_dynamic.py
@@ -19,13 +19,18 @@ class Unet_Dynamic(pl.LightningModule):
 
     def forward(self, x):
         #x = self.model(x)
-        res = 0
-        for x_n in x:
-            if res == 0:
+        res = []
+        for x_n in x[0]:
+            x_n = x_n.unsqueeze(0).unsqueeze(0)
+            if res == []:
                 res = self.model.encoder(x_n)
             else:
-                res += self.model.encoder(x_n)
-        res /= len(x)
+                features_list = self.model.encoder(x_n)
+                for i,feat in enumerate(features_list):
+                    res[i] += feat
+        num_segs = len(x[0])
+        for i,feat in enumerate(res):
+            res[i] /= num_segs
 
         x = self.model.decoder(res)
         if not self.training:
@@ -35,7 +40,7 @@ class Unet_Dynamic(pl.LightningModule):
 
     def _get_reconstruction_loss(self, batch):
         x, y = batch
-        x_hat = self.model(x)
+        x_hat = self(x)
         return self.get_loss(x_hat, y)
 
     def get_loss(self, x, y):
