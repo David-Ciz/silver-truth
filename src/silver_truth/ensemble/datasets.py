4926afd23871bae9b58ec17a8e8f5754ab677983
jocarias <11091450+jocarias@users.noreply.github.com>
2026-02-22 23:32:56 +0000

    Fixed EnsembleDatasetC2 class.
    (cherry picked from commit ba83245093600f24ffe7461a93b6c8696a8c330b)

--- src/silver_truth/ensemble/datasets.py
+++ src/silver_truth/ensemble/datasets.py
@@ -4,7 +4,7 @@ from tqdm import tqdm
 from enum import Enum
 import torch
 from torch.utils.data import Dataset
-import silver_truth.ensemble.external as ext
+import src.silver_truth.ensemble.external as ext
 import albumentations as A
 import numpy as np
 import time
@@ -20,6 +20,19 @@ class Version(Enum):
     C1 = 6  # norm_seg, gt      [1,1]
     C2 = 7  # norm_seg&raw, gt  [2,1]
     D1 = 8  # raw, gt           [1,1]
+
+
+def get_dataset_class(version: Version):
+    if version == Version.B1:
+        return EnsembleDatasetB1
+    elif version == Version.B3:
+        return EnsembleDatasetB3
+    elif version == Version.C1:
+        return EnsembleDatasetC1
+    elif version == Version.C2:
+        return EnsembleDatasetC2
+    else:
+        raise Exception(f"Error: dataset version '{version.name}' not implemented!")
 
 
 class EnsembleDatasetB1(Dataset):
@@ -90,7 +103,7 @@ class EnsembleDatasetB3(Dataset):
         ensemble_parquet_path,
         split,
         transform: Optional[Callable] = None,
-        num_inputs: int = 8,
+        num_inputs: int = 8
     ) -> None:
         super().__init__()
 
@@ -110,40 +123,36 @@ class EnsembleDatasetB3(Dataset):
         for full_cell_id in unique_cell_ids:
             df_same_cell = df[df["full_cell_id"] == full_cell_id]
             segmentations = []
-            gt_image = None
+            gt_image = ()
             # fill tensors with actual data
-            for row in df_same_cell.itertuples():
+            for index, row in enumerate(df_same_cell.itertuples()):
                 if split != "all":
                     if row.split != split:
                         continue
                 # load the image
                 composed_image = tifffile.imread(row.image_path)  # type: ignore
                 # albumentations require (H, W, C) for images
-                segmentations.append(
-                    composed_image[0].astype(dtype=np.float32) / 255
-                )  # scale down to [0,1]
-                gt_image = (
-                    composed_image[1].astype(dtype=np.float32) / 255
-                )  # scale down to [0,1]
+                segmentations.append(composed_image[0].astype(dtype=np.float32) / 255)  # scale down to [0,1]
+                gt_image = (composed_image[1].astype(dtype=np.float32) / 255)  # scale down to [0,1]
 
-            if len(segmentations) > 0 and gt_image is not None:
-                h, w = segmentations[0].shape
-                if len(segmentations) < num_inputs:
-                    for _ in range(num_inputs - len(segmentations)):
-                        segmentations.append(np.zeros((h, w), dtype=np.float32))
-                elif len(segmentations) > num_inputs:
-                    segmentations = segmentations[:num_inputs]
+            if len(segmentations) > 0:
+                h,w = segmentations[0].shape
+                # add empty images to fill all input channels
+                for _ in range(num_inputs - len(segmentations)):
+                    segmentations.append(np.zeros((h,w), dtype=np.float32))
                 self.data.append(np.array(segmentations))
                 self.gts.append(gt_image)
+                
 
     def __len__(self) -> int:
         return len(self.data)
 
     def __getitem__(self, index):
-        c, h, w = self.data[index].shape
-        data = np.reshape(self.data[index], (1, h, w, c))
+        c,h,w = self.data[index].shape
+        data = np.reshape(self.data[index], (1,h,w,c))
         augmented = self.transform(images=data, mask=self.gts[index])
         return augmented["images"], augmented["mask"].unsqueeze(-3).unsqueeze(-3)
+        
 
 
 class EnsembleDatasetC1(Dataset):
@@ -217,36 +226,58 @@ class EnsembleDatasetC2(Dataset):
     Label: crop image of ground truth.
     """
 
-    def __init__(self, ensemble_parquet_path) -> None:
+    def __init__(
+        self,
+        ensemble_parquet_path,
+        split,
+        transform: Optional[Callable] = None,
+    ) -> None:
         super().__init__()
         # load dataframe
         df = ext.load_parquet(ensemble_parquet_path)
 
         self.version = Version.C2
-        # get useful array properties
-        img_count = len(df)
-        img_size = df.iloc[0]["crop_size"]
-
-        # create dataset tensors
-        tensor_shape = (img_count, 2, img_size, img_size)
-        self.data = torch.empty(tensor_shape, dtype=torch.float32)
-        self.gts = torch.empty(tensor_shape, dtype=torch.float32)
+        if transform is None:
+            self.transform = A.Compose([A.ToTensorV2()])
+        else:
+            self.transform = transform
+        self.data = []
+        self.gts = []
 
         # fill tensors with actual data
         for index, row in enumerate(df.itertuples()):
+            if split != "all":
+                if row.split != split:
+                    continue
             # load the image
             composed_image = tifffile.imread(row.image_path)  # type: ignore
             # split the composed image
-            segmentation, gt_image, cell = composed_image
-            self.data[index, 0, :, :] = torch.from_numpy(segmentation)
-            self.data[index, 1, :, :] = torch.from_numpy(cell)
-            self.gts[index, :, :] = torch.from_numpy(gt_image)
+
+            # albumentations require (H, W, C) for images
+            segmentation = (
+                composed_image[0].astype(dtype=np.float32) / 255
+            )  # scale down to [0,1]
+            gt_image = (
+                composed_image[1].astype(dtype=np.float32) / 255
+            )  # scale down to [0,1]
+            raw_image = (
+                composed_image[2].astype(dtype=np.float32) / 255
+            )  # scale down to [0,1]
+            self.data.append(np.array([segmentation,raw_image]))
+            self.gts.append(gt_image)
+
 
     def __len__(self) -> int:
         return len(self.data)
 
     def __getitem__(self, index):
-        return (self.data[index], self.gts[index])
+        c,h,w = self.data[index].shape
+        data = np.reshape(self.data[index], (1,h,w,c))
+        augmented = self.transform(images=data, mask=self.gts[index])
+        return augmented["images"], augmented["mask"].unsqueeze(-3).unsqueeze(-3)
+
+        #augmented = self.transform(image=self.data[index], mask=self.gts[index])
+        #return augmented["image"], augmented["mask"].unsqueeze(-3)
 
 
 def benchmark_EnsembleDataset(path, epochs=1000):
